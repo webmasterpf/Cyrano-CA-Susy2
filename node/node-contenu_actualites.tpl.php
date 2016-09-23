66e9c4a05c20f4c523b86df251c0d439a56fd2d4
webmasterpf-DG <webmaster@provence-formation.com>
2016-09-23 10:37:23 +0200

    DEV contenu VDL/actu : nouveau layout 2 colonnes

--- node/node-contenu_actualites.tpl.php
+++ node/node-contenu_actualites.tpl.php
@@ -3,9 +3,59 @@
   <div class="node-inner">
 <!--______________COLONNE GAUCHE 1________________ -->
   <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
-<div  id="colonne-1" class="col1_layout_3_6_3 actualites_content_G1">
+<div  id="colonne-1" class="col1_layout_9_3 actualites_content_G1">
 
-         <?php if ($node->field_fichier_joint_rp[0]['view']): ?>
+     <?php if ($title): /*insertion du titre de la page et style differencié*/?>
+     <h1 class="titre_actualites_content"><?php print $title; ?></h1>
+         <?php endif; ?>
+     
+        <?php if ($submitted): ?>
+      <span class="submitted"><?php print $submitted; ?></span>
+    <?php endif; ?>
+     
+       <div class="content">
+ <?php
+     /*insertion du contenu du corps de la page*/
+      print $node->content['body']['#value'];
+
+      ?>
+           
+           <?php if ($node->field_choix_galerie_vdl[0]['view']): ?>
+        <div class="galerie-vdl">
+           <?php  print $node->field_choix_galerie_vdl[0]['view'] /**/ ?>
+        </div>
+           <?php endif;?>
+
+        <?php if ($node->field_video_rp[0]['view']): ?>
+        <div id="bloc-video-actualites">
+                   <?php  print $node->field_video_rp[0]['view'] /*Video*/ ?>
+        </div>
+           <?php endif;?>
+
+
+ <?php if ($node->field_fichier_audio_rp[0]['view']): ?>
+        <div id="bloc-audio-actualites">
+                   <?php  print $node->field_fichier_audio_rp[0]['view'] /*audio*/ ?>
+        </div>
+           <?php endif;?>
+
+      
+                 <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_1.php');
+              ?>
+     </div>
+    
+       
+</div><!-- /fin colonne -->
+
+<!--______________COLONNE GAUCHE 2________________ -->
+<div id="colonne-2" class="col2_layout_9_3 actualites_content_G2">
+     
+
+
+       
+  <?php if ($node->field_fichier_joint_rp[0]['view']): ?>
         <div id="bloc-docs-actualites">
             <h3 class="docs-actualites">Informations &amp; Documents joints</h3>
              <table id="table-docs-actualites">
@@ -25,73 +75,12 @@
        </table>
         </div>
            <?php endif;?>
-
-      
- 
-      
-</div>
-<br/>
-<!--______________COLONNE GAUCHE 2________________ -->
-<div id="colonne-2" class="col2_layout_3_6_3 actualites_content_G2">
-         <?php if ($title): /*insertion du titre de la page et style differencié*/?>
-     <h1 class="titre_actualites_content"><?php print $title; ?></h1>
-     <br clear="all"/>
-    <?php endif; ?>
+    
      
-
-     <?php if ($submitted): ?>
-      <span class="submitted"><?php print $submitted; ?></span>
-    <?php endif; ?>
-
-         <?php if ($node->field_choix_galerie_vdl[0]['view']): ?>
-        <div class="galerie-vdl">
-           <?php  print $node->field_choix_galerie_vdl[0]['view'] /**/ ?>
-        </div>
-           <?php endif;?>
-
-        <?php if ($node->field_video_rp[0]['view']): ?>
-        <div id="bloc-video-actualites">
-                   <?php  print $node->field_video_rp[0]['view'] /*Video*/ ?>
-        </div>
-           <?php endif;?>
-
-
- <?php if ($node->field_fichier_audio_rp[0]['view']): ?>
-        <div id="bloc-audio-actualites">
-                   <?php  print $node->field_fichier_audio_rp[0]['view'] /*audio*/ ?>
-        </div>
-           <?php endif;?>
-
-
-
-
-
-       <?php
-              $theme_path = drupal_get_path('theme', 'cyrano_ca'); 
-              include ($theme_path.'/includes/inc_region_col_G2.php');
+                 <?php
+           global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_col_2.php');
               ?>
-     <!-- retour haut selon resolution de l'ecran 
-          <a href="#general" id="retour_haut">Haut de page</a>-->
-</div>
-<!--______________COLONNE GAUCHE 3________________ -->
-   
-<div id="colonne-3" class="col3_layout_3_6_3 actualites_content_G3">
-     <?php print $picture; ?>
-
-   
-
-    <div class="content">
- <?php
-     /*insertion du contenu du corps de la page*/
-      print $node->content['body']['#value'];
-
-      ?>
-       
-
-   
-
-
-    </div>
 
     <?php if ($terms): ?>
       <div class="taxonomy"><?php //print $terms; ?></div>
@@ -100,9 +89,7 @@
     <?php if ($links): ?>
       <div class="links"> <?php //print $links; ?></div>
     <?php endif; ?>
-
-</div>
-   
+</div><!-- /fin colonne -->
 
   </div> <!-- /node-inner -->
 </div> <!-- /node-->
