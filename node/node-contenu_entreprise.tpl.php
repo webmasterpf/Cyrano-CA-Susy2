68efb2103e751334f3c623fa980010e5670d8eb9
webmasterpf-DG <webmaster@provence-formation.com>
2016-10-04 10:20:34 +0200

    DEV contenu entreprise : layout

---
+++ node/node-contenu_entreprise.tpl.php
@@ -0,0 +1,95 @@
+<!--______________NODE TPL POUR TdC CONTENU ENTREPRISE CUSTOM________________ -->
+<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
+  <div class="node-inner">
+<!--______________COLONNE GAUCHE 1________________ -->
+  <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
+<div  class="entreprise_content_G1">
+
+         <?php if ($node->field_fichier_joint_entreprise[0]['view']): ?>
+        <div id="bloc-docs-entreprise">
+            <h3 class="docs-entreprise">Documents joints</h3>
+             <table id="table-docs-entreprise">
+                                <tbody>
+                              
+           <tr class="line1">
+               <td>  <?php  print $node->field_fichier_joint_entreprise[0]['view']  ?></td>
+               <td>  <?php  print $node->field_fichier_joint_entreprise[1]['view']  ?></td>
+               <td>  <?php  print $node->field_fichier_joint_entreprise[2]['view']  ?></td>
+                </tr>
+                 </tbody>
+
+       </table>
+        </div>
+           <?php endif;?>
+
+      
+ 
+      
+</div>
+<br/>
+<!--______________COLONNE GAUCHE 2________________ -->
+<div id="col_G2" class="entreprise_content_G2">
+         <?php if ($title): /*insertion du titre de la page et style differencié*/?>
+     <h1 class="titre_entreprise_content"><?php print $title; ?></h1>
+     <br clear="all"/>
+    <?php endif; ?>
+     
+
+     <?php if ($submitted): ?>
+      <span class="submitted"><?php print $submitted; ?></span>
+    <?php endif; ?>
+
+
+          <?php if ($node->field_choix_galerie_vdl[0]['view']): ?>
+        <div id="bloc-galerie-vdl">
+           <?php  print $node->field_choix_galerie_vdl[0]['view'] /*galerie*/ ?>
+        </div>
+           <?php endif;?>
+
+    <?php
+      global $theme_path;
+      include ($theme_path . '/includes/regions_inc/inc_region_col_2.php');
+      ?>
+</div>
+<!--______________COLONNE GAUCHE 3________________ -->
+   
+<div id="col_G3" class="entreprise_content_G3">
+     <?php print $picture; ?>
+
+   
+
+    <div class="content">
+ <?php
+     /*insertion du contenu du corps de la page*/
+      print $node->content['body']['#value'];
+
+      ?>
+       
+ <?php if ($node->field_video_entreprise[0]['view']): ?>
+        <div id="bloc-video-entreprise">
+                   <?php  print $node->field_video_entreprise[0]['view'] /*Vue actus du lycée*/ ?>
+        </div>
+           <?php endif;?>
+         
+        <?php
+        global $theme_path;
+        include ($theme_path . '/includes/regions_inc/inc_region_col_3.php');
+        ?>
+
+
+    </div>
+
+    <?php if ($terms): ?>
+      <div class="taxonomy"><?php //print $terms; ?></div>
+    <?php endif;?>
+
+    <?php if ($links): ?>
+      <div class="links"> <?php //print $links; ?></div>
+    <?php endif; ?>
+
+</div>
+   
+
+  </div> <!-- /node-inner -->
+</div> <!-- /node-->
+<!--______________FIN NODE TPL CUSTOM________________ -->
