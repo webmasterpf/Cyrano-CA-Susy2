20d8b2aa2265120c5e43dd84704b883edce8b44b
webmasterpf-DG <webmaster@provence-formation.com>
2016-10-10 14:43:45 +0200

    DEV CA: nettoyage page actu globale

--- node/node-page_actualites_globales.tpl.php
+++ node/node-page_actualites_globales.tpl.php
@@ -18,76 +18,69 @@
                 <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
             </div>
             <br />
-            <div id="colonne-1" class="contenu-actu-globale">
+            <div id="colonne-1" class="col1_layout_max contenu-actu-globale">
             <!--______________BLOC REVUE DE PRESSE________________ -->
             <div class="bloc_actu_globale bloc_rp">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_rp.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_rp.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_rp.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_rp.php');
+                ?>
             </div>
 
             <!--______________BLOC ANNONCE________________ -->
             <div class="bloc_actu_globale bloc_annonce">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_annonce.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_annonce.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_annonce.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_annonce.php');
+                ?>
             </div>
 
 
             <!--______________BLOC SORTIE________________ -->
             <div class="bloc_actu_globale bloc_sortie_action">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_sortie.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_sortie.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_sortie.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_sortie.php');
+                ?>
 
             </div>
             <!--______________BLOC PROJET________________ -->
             <div class="bloc_actu_globale bloc_projet">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_projet.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_projet.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_projet.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_projet.php');
+                ?>
             </div>
 
             <!--______________BLOC INNOVATIONS________________ -->
             <div class="bloc_actu_globale bloc_innovation">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_innovation.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_innovation.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_innovation.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_innovation.php');
+                ?>
             </div>
 
             <!--______________BLOC INTERVENANTS________________ -->
             <div class="bloc_actu_globale bloc_intervenant">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_intervenant.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_intervenant.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_intervenant.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_intervenant.php');
+                ?>
             </div>
 
             <!--______________BLOC MINI-STAGE________________ -->
             <div class="bloc_actu_globale bloc_mini_stage">
-                                        <?php
-                                          global $theme_path;
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_mini_stage.php');
-                                        include ($theme_path.'/includes/dedicates_inc/inc_vue_actu_globale_items_mini_stage.php');
-
-                                        ?>
+                <?php
+                global $theme_path;
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_mini_stage.php');
+                include ($theme_path . '/includes/dedicates_inc/inc_vue_actu_globale_items_mini_stage.php');
+                ?>
             </div>
 
           
